70cdec90cc3fd7be81c70b2fc2e30f2eaf551c03
Berardino Chiarello <berardinochiarello@Berardinos-MacBook-Air.fritz.box>
2023-05-19 10:56:35 +0200

    First screen implemented
    Screen where you can select cupcake type, quantity and add special requests

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Berardino Chiarello on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
